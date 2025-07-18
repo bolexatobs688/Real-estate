23a6c5dee598fd363b50e1d84195c7fa0936dc34
bolexatobs688 <bolexatobs@gmail.com>
2025-07-18 23:27:11 +0100

    scroll

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Aboutpage from "./Pages/About/Aboutpage";
 import Contact from "./Pages/Contact/Contact";
 import ProductList from "./Pages/Product/ProductList";
 import Blog from "./Pages/Blog/Blog";
+import ScrollToTop from "./components/Scrolltotop/scrollToTop";
 
 function App() {
   return (
