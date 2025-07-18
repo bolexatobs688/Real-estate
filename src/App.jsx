13fa58af2620ce00ef3724b09b057461ad0f8bfa
bolexatobs688 <bolexatobs@gmail.com>
2025-07-18 17:49:29 +0100

    updated version

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { Routes, Route } from "react-router-dom";
+
 import Header from "./components/Header/Header";
 import Home from "./Pages/Home/Home";
 import Aboutpage from "./Pages/About/Aboutpage";
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,22 +1,29 @@
 import React from "react";
 import Nav from "../Nav/Nav";
 import { FaUser } from "react-icons/fa";
-import { assets } from "../../assets/assets";
 import { Link } from "react-router-dom";
+import logo from "../../assets/logo-png.png";
 
 const Header = () => {
   return (
-    <div className="fixed top-0 left-0 w-full bg-black shadow-md h-28 px-4 z-50 md:px-20 lg:px-32">
+    <div className="fixed top-0 left-0 w-full bg-black shadow-md h-24 px-4 z-50 md:px-20 lg:px-32">
       {/* Header Layout */}
       <div className="flex justify-between items-center h-full">
+        {/* Navigation Toggle */}
         <div>
-          <div>
-            <Nav />
-          </div>
+          <Nav />
         </div>
+
+        {/* Logo (Zoom on Hover) */}
         <Link to="/" className="flex-1 flex justify-center">
-          <img src={assets.logo_dark} className="w-30" alt="Logo" />
+          <img
+            src={logo}
+            className="w-24 md:w-32 transition-transform duration-300 hover:scale-105 object-contain py-2"
+            alt="Logo"
+          />
         </Link>
+
+        {/* Right Side Icon */}
         <div className="flex items-center space-x-2 text-gray-700">
           <FaUser className="text-green-600 w-5 h-5 mr-10" />
         </div>
